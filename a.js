b16453dbbe3ade633311994bb3e1482025cf33b6
shinichi kogai <kogai0121@gmail.com>
2017-08-04 09:50:50 +0900

    try opaque type

---
+++ a.js
@@ -0,0 +1,7 @@
+// @flow
+
+export opaque type A = string
+export type B = string
+
+const a: A = 'a has alias of string type';
+const b: B = 'a has alias of string type';
---
+++ b.js
@@ -0,0 +1,9 @@
+// @flow
+
+import type {
+  A,
+  B
+} from './a';
+
+const a: A = "string"
+const b: B = "string"
